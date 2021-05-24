c3f910019953b9afbda7fa3f65cf652cdd4e4c77
Jaime1998 <jaime.cuartas@correounivalle.edu.co>
2021-05-23 23:24:37 -0500

    JScrollPane added

--- src/View/Window.java
+++ src/View/Window.java
@@ -8,22 +8,20 @@ public class Window extends JFrame {
 
 
     private JButton buttonUpdate;
-    private JPanel generalPanel;
-    private JPanel RightPanel;
-    private JScrollPane scrollDeclaration;
+    private JButton buttonSave;
     private JTextArea areaDeclaration;
 
     public Window() {
-        this.setSize(500,500);
+        this.setSize(1000,700);
         this.setMinimumSize(new Dimension(500,500));
         this.setLocationRelativeTo(null);
         initComponents();
-
         this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
     }
     private void initComponents(){
         this.getContentPane().setLayout(new GridBagLayout());
-        JTextArea areaTexto = new JTextArea("//Declaration");
+        areaDeclaration  = new JTextArea("//Declaration");
+        JScrollPane scrollDeclaration = new JScrollPane(areaDeclaration);
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.gridx = 0;
         constraints.gridy = 0;
@@ -31,10 +29,10 @@ public class Window extends JFrame {
         constraints.gridheight = 2;
         constraints.fill = GridBagConstraints.BOTH;
         constraints.weighty = 1.0;
-        this.getContentPane().add (areaTexto, constraints);
+        this.getContentPane().add (scrollDeclaration, constraints);
         constraints.weighty = 0.0;
 
-        JButton boton1 = new JButton ("Update Model");
+        buttonUpdate = new JButton ("Update Model");
         constraints.gridx = 2;
         constraints.gridy = 0;
         constraints.gridwidth = 1;
@@ -43,20 +41,18 @@ public class Window extends JFrame {
         constraints.weightx = 0.4;
         constraints.anchor = GridBagConstraints.NORTH;
         constraints.fill = GridBagConstraints.NONE;
-        this.getContentPane().add (boton1, constraints);
+        this.getContentPane().add (buttonUpdate, constraints);
         constraints.weighty = 0.0;
         constraints.weightx = 0.0;
 
-
-
-        JButton boton3 = new JButton ("Save");
+        buttonSave = new JButton ("Save");
         constraints.gridx = 0;
         constraints.gridy = 2;
         constraints.gridwidth = 1;
         constraints.gridheight = 1;
-        this.getContentPane().add (boton3, constraints);
+        this.getContentPane().add (buttonSave, constraints);
 
-        JTextField campoTexto = new JTextField ("Directory");
+        JTextField pathDirectory = new JTextField ("Directory");
         constraints.gridx = 1;
         constraints.gridy = 2;
         constraints.gridwidth = 1;
@@ -64,68 +60,9 @@ public class Window extends JFrame {
         constraints.weightx = 1.0;
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.anchor = GridBagConstraints.CENTER;
-        this.getContentPane().add (campoTexto, constraints);
+        this.getContentPane().add (pathDirectory, constraints);
         constraints.weightx = 0.0;
     }
-
-    private void initComponents2(Container pane){
-        if (false) {
-            pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-        }
-
-        JButton button;
-        pane.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        if (true) {
-            //natural height, maximum width
-            c.fill = GridBagConstraints.HORIZONTAL;
-        }
-
-        button = new JButton("Button 1");
-        if (true) {
-            c.weightx = 0.5;
-        }
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 0;
-        pane.add(button, c);
-
-        button = new JButton("Button 2");
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = 1;
-        c.gridy = 0;
-        pane.add(button, c);
-
-        button = new JButton("Button 3");
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = 2;
-        c.gridy = 0;
-        pane.add(button, c);
-
-        button = new JButton("Long-Named Button 4");
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.ipady = 40;      //make this component tall
-        c.weightx = 0.0;
-        c.gridwidth = 3;
-        c.gridx = 0;
-        c.gridy = 1;
-        pane.add(button, c);
-
-        button = new JButton("5");
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.ipady = 0;       //reset to default
-        c.weighty = 1.0;   //request any extra vertical space
-        c.anchor = GridBagConstraints.PAGE_END; //bottom of space
-        c.insets = new Insets(10,0,0,0);  //top padding
-        c.gridx = 1;       //aligned with button 2
-        c.gridwidth = 2;   //2 columns wide
-        c.gridy = 2;       //third row
-        pane.add(button, c);
-    }
-
-
 
     public static void main(String args[]) {
         /* Create and display the form */
